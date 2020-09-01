f3626bebeac9261645d60c8d1e59e4169ca8fa25
Chew <chew@chew.pw>
2020-09-01 16:43:52 -0500

    Channel mapping and join correct channels

--- src/main/java/pw/chew/multirc/Main.java
+++ src/main/java/pw/chew/multirc/Main.java
@@ -1,24 +1,29 @@
 package pw.chew.multirc;
 
 import org.kitteh.irc.client.library.Client;
+import pw.chew.multirc.listeners.ChannelMessageHandler;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class Main {
+    public static Client server1, server2;
+
     public static void main(String[] args) throws IOException {
         Properties server1config = new Properties();
         server1config.load(new FileInputStream("server1.properties"));
         Properties server2config = new Properties();
         server2config.load(new FileInputStream("server2.properties"));
 
-        Client server1 = create(server1config);
-        Client server2 = create(server2config);
+        server1 = create(server1config);
+        server2 = create(server2config);
     }
 
     public static Client create(Properties properties) {
-        return Client.builder()
+        Client client = Client.builder()
                 .nick(properties.getProperty("nickname"))
                 .realName(properties.getProperty("realname"))
                 .user(properties.getProperty("username"))
@@ -27,5 +32,14 @@ public class Main {
                 .port(Integer.parseInt(properties.getProperty("port")), Client.Builder.Server.SecurityType.SECURE)
                 .then()
                 .buildAndConnect();
+
+        String[] mappings = properties.getProperty("mapping").split(",");
+        List<String> channels = new ArrayList<>();
+        for(String mapping : mappings)
+            channels.add(mapping.split(":")[0]);
+        client.addChannel(channels.toArray(new String[0]));
+        client.getEventManager().registerEventListener(new ChannelMessageHandler());
+
+        return client;
     }
 }
