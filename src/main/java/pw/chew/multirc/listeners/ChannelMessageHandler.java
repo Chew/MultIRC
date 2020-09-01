067b81f5d61dc8b136754e1f4fb7630408330ce5
Chew <chew@chew.pw>
2020-09-01 17:36:56 -0500

    Support for ACTION ctcp

--- src/main/java/pw/chew/multirc/listeners/ChannelMessageHandler.java
+++ src/main/java/pw/chew/multirc/listeners/ChannelMessageHandler.java
@@ -1,9 +1,13 @@
 package pw.chew.multirc.listeners;
 
 import net.engio.mbassy.listener.Handler;
+import org.kitteh.irc.client.library.event.channel.ChannelCtcpEvent;
 import org.kitteh.irc.client.library.event.channel.ChannelMessageEvent;
 import org.kitteh.irc.client.library.util.Format;
 import pw.chew.multirc.Main;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ChannelMessageHandler {
     private static final Format[] COLORS = {
@@ -22,11 +26,27 @@ public class ChannelMessageHandler {
 
     @Handler
     public void onChannelMessage(ChannelMessageEvent event) {
+        if(event.getActor().getNick().equals(event.getActor().getClient().getNick()))
+            return;
+
         String message = getDisplayName(event.getActor().getNick()) + " " + event.getMessage();
         if(event.getClient() == Main.server1) {
             Main.server2.sendMessage(Main.mapping1.get(event.getChannel().getName()), message);
         } else {
             Main.server1.sendMessage(Main.mapping2.get(event.getChannel().getName()), message);
+        }
+    }
+
+    @Handler
+    public void onChannelCTCP(ChannelCtcpEvent event) {
+        if(event.getCommand().equalsIgnoreCase("action")) {
+            String message = Format.ITALIC + event.getActor().getNick() + " " + event.getMessage().substring(7);
+
+            if(event.getClient() == Main.server1) {
+                Main.server2.sendMessage(Main.mapping1.get(event.getChannel().getName()), message);
+            } else {
+                Main.server1.sendMessage(Main.mapping2.get(event.getChannel().getName()), message);
+            }
         }
     }
 
